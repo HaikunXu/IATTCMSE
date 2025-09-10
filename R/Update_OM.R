3e558308eafc3b3fce19177616acc9efd9a6b6e4
Haikun Xu <haikunxu1990@gmail.com>
2025-09-10 10:44:46 -0700

    fix a bug in the directory of data file

--- R/Update_OM.R
+++ R/Update_OM.R
@@ -12,25 +12,21 @@ Update_OM = function(dir_OM, dir_OM_Boot, Mcycle) {
   
   # change data file
   dat_OM <- r4ss::SS_readdat_3.30(file = paste0(dir_OM, "BET-EPO.dat"), verbose = FALSE)
+  dat_OM_Boot <- r4ss::SS_readdat_3.30(file = paste0(dir_OM_Boot, "BET-EPO.dat"), verbose = FALSE)
   data_true <- r4ss::SS_readdat_3.30(file = paste0(dir_OM_Boot, "data_expval.ss"), verbose = FALSE)
   
-  # add new CPUE
+    # add new CPUE
   CPUE <- dat_OM$CPUE
-  CPUE_boot <- data_true$CPUE[(nrow(CPUE)+1):nrow(data_true$CPUE),]
-  CPUE_new <- rbind(CPUE, CPUE_boot)
-  
-  # add new catch
-  catch <- dat_OM$catch
-  catch_boot <- data_true$catch[which(data_true$catch$year>max(catch$year)),]
-  catch_new <- dplyr::arrange(rbind(catch, catch_boot), fleet, year)
+  CPUE_true <- data_true$CPUE[(nrow(CPUE)+1):nrow(data_true$CPUE),]
+  CPUE_new <- rbind(CPUE, CPUE_true)
   
   # add new LF
   LF <- dat_OM$sizefreq_data_list[[1]]
-  LF_boot <- dplyr::filter(data_true$sizefreq_data_list[[1]], year > max(LF$year))
-  LF_new <- dplyr::arrange(rbind(LF, LF_boot), fleet, year)
+  LF_true <- dplyr::filter(data_true$sizefreq_data_list[[1]], year > max(LF$year))
+  LF_new <- dplyr::arrange(rbind(LF, LF_true), fleet, year)
   
   # save data file
-  dat_OM$catch <- catch_new
+  dat_OM$catch <- dat_OM_Boot$catch
   dat_OM$CPUE <- CPUE_new
   dat_OM$sizefreq_data_list[[1]] <- LF_new
   dat_OM$Nobs_per_method <- nrow(LF_new)
