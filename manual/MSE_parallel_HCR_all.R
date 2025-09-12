c4e5d47cb364e78d79447db151aa8963b78abf18
Haikun Xu <haikunxu1990@gmail.com>
2025-09-12 13:47:54 -0700

    clean up the code space

--- manual/MSE_parallel_HCR_all.R
+++ manual/MSE_parallel_HCR_all.R
@@ -9,8 +9,8 @@ pdir <- "D:/OneDrive - IATTC/IATTC/2025/MSE/Test/"
 sdir <- "D:/OneDrive - IATTC/IATTC/2025/SAC16/BET F30/"
 
 # Dimensions
-niterations <- 10
-nyears <- 15
+niterations <- 12
+nyears <- 18
 nquarters <- nyears * 4
 Mcycle <- 3
 nsteps <- nyears / Mcycle
@@ -22,20 +22,6 @@ dat_name <- "BET-EPO.dat"
 ctl_name <- "BET-EPO.ctl"
 ss_name <- "ss.exe"
 
-# simulate and save recruitment devs
-# set.seed(123)
-# seeds <- sample(1:1e3, size = niterations, replace = FALSE)  # Sample 5 elements without replacement
-# write.csv(seeds, file = paste0(pdir,"seeds.csv"), row.names = FALSE)
-# 
-# R_devs <- matrix(NA, nrow = nquarters, ncol = niterations)
-# for (i in 1:niterations) {
-#   set.seed(seeds[i])
-#   R_devs[,i] <- rnorm(n = nquarters, mean = 0, sd = 0.6) - 0.6 ^ 2 / 2
-# }
-# 
-# write.csv(R_devs, file = paste0(pdir,"R_devs.csv"), row.names = FALSE)
-
-
 # Calculate the numbers of cores 
 no_cores = 12 # detectCores() - 2
 # Initiate cluster
@@ -43,7 +29,7 @@ cl = makeCluster(no_cores)
 registerDoParallel(cl)
 
 
-OM_name <- c("Fix-1-1", "Sel-1-1", "Gro-1-1", "Mrt-1-1") #, "Fix-1.01-1", "Fix-1.02-1")
+OM_name <- c("Fix-1-1", "Sel-1-1", "Gro-1-1", "Mrt-1-1")[1] #, "Fix-1.01-1", "Fix-1.02-1")
 OM <- paste0(OM_name, "/")
 HCR_name <- c("HCR_staff", "HCR_staff_0", "HCR_staff_0_Fscaler", "HCR_staff_Fscaler")[2]
 HCR <- paste0(HCR_name, "/")
@@ -67,7 +53,7 @@ for (HCRnum in 1:length(HCR)) {
 # specify the run list 
 runs <- data.frame(expand.grid(run_hcr = HCR, run_om = OM, run_itr = 1:niterations))
 
-# i = 33;  HCR= runs[i,1]; OM = runs[i,2]; itrnum= runs[i,3]
+# i = 9;  HCR= runs[i,1]; OM = runs[i,2]; itrnum= runs[i,3]
 # BET_MSE(pdir,sdir,HS,runs[i, 1],runs[i, 2],runs[i, 3],nquarters,Mcycle,n_extra_R,startquarter,endquarter,EM_comp_fleet,dat_name,ctl_name,ss_name,plot=TRUE)
 
 foreach(i = 1:nrow(runs)) %dopar% {
---
+++ manual/Rdevs_seeds.R
@@ -0,0 +1,12 @@
+# simulate and save recruitment devs for 100 iterations and 200 quarters
+# set.seed(123)
+# seeds <- sample(1:1e3, size = 100, replace = FALSE)  # Sample 5 elements without replacement
+# write.csv(seeds, file = paste0(pdir,"seeds.csv"), row.names = FALSE)
+# 
+# R_devs <- matrix(NA, nrow = 200, ncol = 100)
+# for (i in 1:100) {
+#   set.seed(seeds[i])
+#   R_devs[,i] <- rnorm(n = 200, mean = 0, sd = 0.6) - 0.6 ^ 2 / 2
+# }
+# 
+# write.csv(R_devs, file = paste0(pdir,"R_devs.csv"), row.names = FALSE)
